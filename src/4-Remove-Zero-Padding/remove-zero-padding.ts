3b423cae967e1f31652e26a89d36ebd90172e3c1
Eric Potter <eric_potter@sweetwater.com>
2024-08-01 18:20:16 -0400

    4-Remove-Zero-Padding-challenge

---
+++ src/4-Remove-Zero-Padding/remove-zero-padding.ts
@@ -0,0 +1,10 @@
+/*
+Given a non-empty array containing only non-negative integers, return the array 
+with trailing and leading zeroes removed. 
+*/
+
+export const READY_TO_TEST = false; // SET ME TO TRUE IF YOU DARE
+
+export function RemovePadding(input: number[]): number[] {
+  return []; // TODO - IMPLEMENT
+}
